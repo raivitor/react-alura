6b24a0cdb133f291426336c330dcfc2b8b9ebc05
Rai Vitor <raivitor@gmail.com>
2018-01-17 11:54:46 -0300

    Add rotas/navegação ao SPA

--- react-alura/src/index.js
+++ react-alura/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import AutorBox from './componentes/Autor';
+import Home from './componentes/Home';
+import './index.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <Router>
+        <App>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/autor" component={AutorBox} />
+                <Route path="/livro" />
+            </Switch>
+        </App>
+    </Router>
+
+), document.getElementById('root'));
 registerServiceWorker();
