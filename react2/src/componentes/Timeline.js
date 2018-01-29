5ed6c235592658f60fafbc8744b62be182712d02
Rai Vitor <raivitor@gmail.com>
2018-01-29 10:43:45 -0300

    Add navegação entre os links de usuários

--- react2/src/componentes/Timeline.js
+++ react2/src/componentes/Timeline.js
@@ -3,17 +3,37 @@ import FotoItem from './Foto';
 
 export default class Timeline extends Component {
 
-    constructor(){
-      super();
+    constructor(props){
+      super(props);
       this.state = {fotos:[]};
+      this.login = this.props.login;
+    }
+
+    carregaFotos(){  
+      let urlPerfil;
+
+      if(this.login === undefined) {
+        urlPerfil = `http://localhost:8080/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`;
+      } else {
+        urlPerfil = `http://localhost:8080/api/public/fotos/${this.login}`;
+      }
+
+      fetch(urlPerfil)
+       .then(response => response.json())
+       .then(fotos => {         
+         this.setState({fotos:fotos});
+       });      
     }
 
     componentDidMount(){
-      fetch(`http://localhost:8080/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`)
-       .then(response => response.json())
-       .then(fotos => {
-         this.setState({fotos:fotos});
-       });
+      this.carregaFotos();
+    }
+
+    componentWillReceiveProps(nextProps){
+      if(nextProps.login !== undefined){
+        this.login = nextProps.login;
+        this.carregaFotos();
+      }
     }
 
     render(){
